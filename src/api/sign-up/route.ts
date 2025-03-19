dfe357b0c1ac85d3df467178c15d59a1fbaf78a2
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-03-19 22:46:21 +0530

    During signup applying if else condition to check user in DB

--- src/api/sign-up/route.ts
+++ src/api/sign-up/route.ts
@@ -9,6 +9,40 @@ export async function POST(request: Request) {
 
   try {
     const { username, email, password } = await request.json();
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "username is already taken ",
+        },
+        { status: 400 }
+      );
+    }
+    const existingUserByEmail = await UserModel.findOne({ email });
+
+    if (existingUserByEmail) {
+    } else {
+      const hashPasssword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashPasssword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+      await newUser.save();
+    }
   } catch (error) {
     console.log("Error Registering User", error);
     return Response.json(
